a63bd734d1284aad7bfcdba036eaacea70eab1b1
sky0621 <sky14work@gmail.com>
2023-08-19 21:24:08 +0900

    -

--- erc20/test/erc20.test.ts
+++ erc20/test/erc20.test.ts
@@ -12,4 +12,24 @@ describe("ERC20 contract states", function () {
         expect(await erc20.decimals()).to.equal(18)
         expect(await erc20.totalSupply()).to.equal(0)
     })
+
+    it("owner is account0", async function () {
+        const [account0] = await ethers.getSigners()
+        const ERC20 = await ethers.getContractFactory("ERC20")
+        const erc20 = await ERC20.deploy("Zenny", "ZNY", 18)
+        await erc20.waitForDeployment()
+
+        expect(account0.address).to.equal("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+        expect(await erc20._owner()).to.equal(account0.address)
+    })
+
+    it("owner is account1", async function () {
+        const [account0, account1] = await ethers.getSigners()
+        const ERC20 = await ethers.getContractFactory("ERC20", account1)
+        const erc20 = await ERC20.deploy("Zenny", "ZNY", 18)
+        await erc20.waitForDeployment()
+
+        expect(account1.address).to.equal("0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
+        expect(await erc20._owner()).to.equal(account1.address)
+    })
 })
