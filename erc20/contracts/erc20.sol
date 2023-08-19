25fb1be31be25c5df82f821d2b9161aca9cb79bb
sky0621 <sky14work@gmail.com>
2023-08-19 23:27:09 +0900

    -

--- erc20/contracts/erc20.sol
+++ erc20/contracts/erc20.sol
@@ -6,7 +6,8 @@ contract ERC20 {
     string private _name;
     string private _symbol;
     uint8 private _decimals;
-    address public _owner;  // temporary public
+    address private _owner;
+    mapping(address => uint256) private _balances;
 
     constructor (string memory name_, string memory symbol_, uint8 decimals_) {
         _name = name_;
@@ -29,5 +30,23 @@ contract ERC20 {
 
     function totalSupply() external view returns (uint256) {
         return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function mint(address account, uint256 amount) external {
+        require(msg.sender == _owner, "only contract owner can call mint");
+        require(account != address(0), "mint to the zero address is not allowed");
+        _totalSupply = _totalSupply + amount;
+        _balances[account] = _balances[account] + amount;
+    }
+
+    function burn(address account, uint256 amount) external {
+        require(msg.sender == _owner, "only contract owner can call burn");
+        require(account != address(0), "burn to the zero address is not allowed");
+        _totalSupply = _totalSupply - amount;
+        _balances[account] = _balances[account] - amount;
     }
 }
--- erc20/test/erc20.test.ts
+++ erc20/test/erc20.test.ts
@@ -1,35 +1,68 @@
 import {ethers} from "hardhat";
 import {expect} from "chai"
+import {loadFixture} from "@nomicfoundation/hardhat-network-helpers"
+
+async function deployContractsFixture() {
+    const [account0, account1, account2] = await ethers.getSigners();
+    const ERC20 = await ethers.getContractFactory("ERC20", account0);
+    const erc20 = await ERC20.deploy("Zenny", "ZNY", 18);
+    await erc20.waitForDeployment()
+    return {erc20, account0, account1, account2}
+}
 
 describe("ERC20 contract states", function () {
     it("getters", async function () {
-        const ERC20 = await ethers.getContractFactory("ERC20")
-        const erc20 = await ERC20.deploy("Zenny", "ZNY", 18)
-        await erc20.waitForDeployment()
+        const {erc20} = await loadFixture(deployContractsFixture)
 
         expect(await erc20.name()).to.equal("Zenny")
         expect(await erc20.symbol()).to.equal("ZNY")
         expect(await erc20.decimals()).to.equal(18)
         expect(await erc20.totalSupply()).to.equal(0)
     })
+})
 
-    it("owner is account0", async function () {
-        const [account0] = await ethers.getSigners()
-        const ERC20 = await ethers.getContractFactory("ERC20")
-        const erc20 = await ERC20.deploy("Zenny", "ZNY", 18)
-        await erc20.waitForDeployment()
+describe("ERC20 mint", function () {
+    it("mint", async function () {
+        const {erc20, account0, account1, account2} = await loadFixture(deployContractsFixture)
 
-        expect(account0.address).to.equal("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
-        expect(await erc20._owner()).to.equal(account0.address)
+        expect(await erc20.balanceOf(account0.address)).to.equal(0)
+        expect(await erc20.balanceOf(account1.address)).to.equal(0)
+        expect(await erc20.balanceOf(account2.address)).to.equal(0)
+
+        const decimals = await erc20.decimals();
+
+        const amount1: bigint = 10n * (10n ** decimals)
+        const tx1 = await erc20.mint(account1.address, amount1)
+        const receipt1 = await tx1.wait();
+
+        expect(await erc20.balanceOf(account0.address)).to.equal(0)
+        expect(await erc20.balanceOf(account1.address)).to.equal(amount1)
+        expect(await erc20.balanceOf(account2.address)).to.equal(0)
+        expect(await erc20.totalSupply()).to.equal(amount1)
+
+        const amount2: bigint = 10n * (10n ** decimals)
+        const tx2 = await erc20.mint(account1.address, amount2)
+        const receipt2 = await tx2.wait();
+
+        expect(await erc20.balanceOf(account0.address)).to.equal(0)
+        expect(await erc20.balanceOf(account1.address)).to.equal(amount1 + amount2)
+        expect(await erc20.balanceOf(account2.address)).to.equal(0)
+        expect(await erc20.totalSupply()).to.equal(amount1 + amount2)
     })
 
-    it("owner is account1", async function () {
-        const [account0, account1] = await ethers.getSigners()
-        const ERC20 = await ethers.getContractFactory("ERC20", account1)
-        const erc20 = await ERC20.deploy("Zenny", "ZNY", 18)
-        await erc20.waitForDeployment()
+    it("mint from non-owner", async function () {
+        const {erc20, account0, account1, account2} = await loadFixture(deployContractsFixture)
+        const amount = 123;
 
-        expect(account1.address).to.equal("0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
-        expect(await erc20._owner()).to.equal(account1.address)
+        await expect(erc20.connect(account1).mint(account1.address, amount)).to.be.revertedWith("only contract owner can call mint")
+        expect(await erc20.balanceOf(account1.address)).to.equal(0);
+    })
+
+    it("mint overflow", async function () {
+        const {erc20, account0, account1, account2} = await loadFixture(deployContractsFixture)
+        const uint256Max: bigint = 2n ** 256n - 1n;
+
+        await erc20.mint(account1.address, uint256Max)
+        await expect(erc20.mint(account1.address, 1)).to.be.revertedWithPanic(0x11)
     })
 })
